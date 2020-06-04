5f50a837c6a5b6259ad05ef3609d2600f7e9095c
robiwankenobi <rfnkurle@gmail.com>
2020-06-04 16:40:27 -0600

    App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Row, Col} from "reactstrap";
+import SearchBar from "./components/SearchBar"
+import DayDetails from "./components/DayDetails"
+import DayCard from "./components/DayCard"
+
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col md={7}>
+          <h1>Weather Info for LOCATION</h1>
+        </Col>
+        <Col md={5}>
+          <SearchBar/>
+        </Col>
+      </Row>
+      <Row></Row>
+      <Row>
+    <Col>
+    <DayDetails></DayDetails>
+    </Col>
+  </Row>
+    </Container>
   );
 }
 
