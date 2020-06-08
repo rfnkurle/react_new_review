d755f3d38370da053f9f1e30a38bfe83aeb8402a
robiwankenobi <rfnkurle@gmail.com>
2020-06-08 16:29:56 -0600

    added props to daycard

--- src/components/DayCard.js
+++ src/components/DayCard.js
@@ -1,9 +1,22 @@
 import React from "react";
+import {Col, Card, CardHeader, CardBody} from 'reactstrap'
 
 
 const DayCard = props => {
+    console.log(props)
     return(
-        <p>DayCard</p>
+        <Col>
+        <Card>
+        <CardHeader>{props.day}</CardHeader>
+        <CardBody>
+           <h3>{props.temp}°</h3> 
+           <img src={`${process.env.PUBLIC_URL}/icons/${props.icon}.png`} alt={props.description}/>
+        <p><strong>High:</strong> {props.high}°</p>
+        <p><strong>Low:</strong> {props.low}°</p>
+        <p><strong>Precip:</strong> {props.precip}%</p>
+        </CardBody>
+        </Card>
+        </Col>
     )
 }
 
