acd0d098bccfeeac0d73631529cb6b5f1837ff40
robiwankenobi <rfnkurle@gmail.com>
2020-06-14 20:28:25 -0600

    added api env notes

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 
-//must be preceded with REACT_APP_etc.
+//must be brought in from process.env and preceded with REACT_APP_etc.
+//env file which put into git ignore makes sure to hide your api keys so they don't got to gihub
+//but better to be on backend
 const googleKey = process.env.REACT_APP_GOOGLE_KEY;
 const weatherKey = process.env.REACT_APP_WEATHER_KEY;
 
