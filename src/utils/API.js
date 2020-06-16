c76d2d1f3988e80a0003378bc6e8713dcb62893f
robiwankenobi <rfnkurle@gmail.com>
2020-06-16 16:04:50 -0600

    lat/long

--- src/utils/API.js
+++ src/utils/API.js
@@ -12,8 +12,12 @@ export default {
             console.log(googleKey, weatherKey)
         //returning the axios call lets user do more
             return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${googleKey}`)
-            .then(res=> 
-                {console.log(res)})
+            .then(res=> {
+                console.log(res)
+                //destructured with lat and lng (what it is called in google) to lon (what it is called in weatherbit)
+                    const {lat, lng: lon} = res.data.results[0].geometry.location;
+                    console.log(lat, lon)
+                })
             .catch(err => {
                 console.log(err)
             })
